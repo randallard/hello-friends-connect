c597eb113c3c2be8b953da7f7cbe6641b2258d93
Ryan Khetlyr <randallard@yahoo.com>
2025-02-01 08:18:04 -0800

    added FriendsConnect component to App

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use leptos::*;
 use wasm_bindgen_test::*;
 
 pub mod connect_component;
+use connect_component::FriendsConnect;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
@@ -9,6 +10,7 @@ wasm_bindgen_test_configure!(run_in_browser);
 pub fn App() -> impl IntoView {
     view! { 
         <div>"Hello"</div>
+        <FriendsConnect />
     }
 }
 
@@ -23,4 +25,20 @@ fn test_app_says_hello() {
         .unwrap();
         
     assert_eq!(div.text_content().unwrap(), "Hello");
+}
+
+#[wasm_bindgen_test]
+fn test_app_contains_friends_connect() {
+    mount_to_body(|| view! { <App/> });
+    
+    // Look for the heading that should be inside FriendsConnect
+    let friends_connect_heading = document()
+        .query_selector("h2")
+        .unwrap()
+        .expect("Should find FriendsConnect heading");
+        
+    assert_eq!(
+        friends_connect_heading.text_content().unwrap(),
+        "Connect with Friends"
+    );
 }
